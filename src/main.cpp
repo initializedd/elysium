358a6515ffe50671c3805208ac1c74a88240b4a7
initializedd <119122691+initializedd@users.noreply.github.com>
2025-11-09 18:57:38 +0000

    Add key callback

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,12 @@
 
 #include "systems/sound_system.hpp"
 
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
 int main() {
     vk::detail::DispatchLoaderDynamic default_dispatch_loader_dynamic;
     default_dispatch_loader_dynamic.init();
@@ -69,6 +75,7 @@ int main() {
     }
 
     glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
 
     while(!glfwWindowShouldClose(window)) {
         glfwSwapBuffers(window);
